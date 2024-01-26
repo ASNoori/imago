3547aa7352e349087fff2e569840aaef478ef57c
ASNoori <nooriameer12@gmail.com>
2024-01-26 12:40:19 +0530

    download feture update

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -13,8 +13,14 @@ import {
 import DownloadForOfflineIcon from "@mui/icons-material/DownloadForOffline";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { saveAs } from "file-saver";
+import MaterialModal from "../contributor components/Modal";
 
-const ImageGallery = ({ searchResults,favorites,toggleFavorite}) => {
+const ImageGallery = ({ searchResults, favorites, toggleFavorite }) => {
+  // for image modal
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isimageModalOpen, setImageModalOpen] = useState(false);
+  // -----------------------
+
   console.log(searchResults);
   // const [images, setImages] = useState([]);
 
@@ -91,11 +97,31 @@ const ImageGallery = ({ searchResults,favorites,toggleFavorite}) => {
 
     return new Blob(byteArrays, { type: contentType });
   };
+
+  // open image modal
+  const openImageModal = (image) => {
+    setSelectedImage(image);
+    setImageModalOpen(true);
+  };
+  // close image modal
+  const closeImageModal = () => {
+    setImageModalOpen(false);
+  };
+  // handle share url
+  const handleShareUrl=async() =>{
+      try{
+          const urlToCopy = window.location.href;
+          await navigator.clipboard.writeText(urlToCopy);
+          alert('URL copied to clipboard!');
+      }
+      catch(err){
+          console.error('Unable to copy url to clipboard',err);
+      }
+  }
   return (
-    
     <Box id="imagegallery">
       <Grid container spacing={3} p={6}>
-      {/* {!searchResults.length && (
+        {/* {!searchResults.length && (
           <p style={{ textAlign: 'center'}}>Loading...</p>
         )} */}
         {searchResults.map((image, index) => (
@@ -119,6 +145,8 @@ const ImageGallery = ({ searchResults,favorites,toggleFavorite}) => {
                   // e.target.style.height = `${calculatedHeight}px`;
                   e.target.style.height = "500px";
                 }}
+                // will open image modal
+                onClick={() => openImageModal(image)}
               />
               <CardContent
                 sx={{ display: "flex", justifyContent: "space-between" }}
@@ -140,6 +168,43 @@ const ImageGallery = ({ searchResults,favorites,toggleFavorite}) => {
                 />
               </CardContent>
             </Card>
+            <Box>
+              <MaterialModal
+                title={selectedImage ? selectedImage.title : ""}
+                open={isimageModalOpen}
+                onClose={closeImageModal}
+              >
+                <Grid container spacing={2}>
+                  <Grid item xs={8}>
+                    {selectedImage && (
+                      <img
+                        src={selectedImage.image}
+                        alt={selectedImage.title}
+                        style={{ maxWidth: "100%", maxHeight: "100%" }}
+                      />
+                    )}
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      sx={{
+                        mt: 3,
+                        mb: 2,
+                        background: "#000",
+                        "&:hover": { background: "#000" },
+                      }}
+                      onClick={() => handleDownload(selectedImage)}
+                    >
+                      Download
+                    </Button>
+                    <Button onClick={() => handleShareUrl()}>
+                      Share
+                    </Button>
+                  </Grid>
+                </Grid>
+              </MaterialModal>
+            </Box>
           </Grid>
         ))}
       </Grid>
